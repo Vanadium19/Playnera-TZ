b0b52dc24e405d8af33d36fa2ad1bc874fa1a624
Vladislav Melnik <GimliVlad@list.ru>
2025-03-13 12:18:05 +0300

    Add folders

--- Assets/Game/Scripts/Gameplay/GameSystems/Controllers/DragAndDropController.cs
+++ Assets/Game/Scripts/Gameplay/GameSystems/Controllers/DragAndDropController.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace Game.GameSystems
+namespace Game.GameSystems.Controllers
 {
     public class DragAndDropController : MonoBehaviour,
         IPointerDownHandler,
